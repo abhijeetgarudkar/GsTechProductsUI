a38a6f07359e603bff2f57aa2fab05b4c18e9977
Abhijeet Garudkar <abhijeetgarudkar28@gmail.com>
2026-01-04 00:25:32 +0530

    refactor: update PROD backend URL and Netlify configuration

--- dealerapp/assets/js/config.js
+++ dealerapp/assets/js/config.js
@@ -1,6 +1,7 @@
 (() => {
   window.AppConfig = {
-    BACKEND_BASE_URL: "http://localhost:8080"
+    //BACKEND_BASE_URL: "http://localhost:8080"
+    BACKEND_BASE_URL: "https://gstechproductsbackend.onrender.com"
   };
 
   console.log("AppConfig initialized (direct backend mode)");
